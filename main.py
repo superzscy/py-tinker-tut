a0a33570f09f946a8deeefcff358bc6ae64d7313
Jack Chen <476100664@qq.com>
2024-12-14 01:21:52 +0800

    Update

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ def open_openfilename_dialog(var):
     var.set(f"{openfilename}")
 
 
-path_summary_sheet_var = StringVar()
-path_summary_sheet_var.set("NA")
-
+summary_sheet_path_var = StringVar()
+summary_sheet_path_var.set("NA")
 summary_sheet_label_var = StringVar()
-summary_sheet_label_var.set("NA")
+summary_sheet_label_var.set("集采第九批内部统计使用")
 
 frame_summary = Frame(root)
 frame_summary.pack(fill=BOTH, expand=True)
@@ -30,14 +29,14 @@ frame_summary.pack(fill=BOTH, expand=True)
 btn_choose_summary_sheet = Button(
     frame_summary,
     text="选择汇总表",
-    command=lambda: open_openfilename_dialog(path_summary_sheet_var),
+    command=lambda: open_openfilename_dialog(summary_sheet_path_var),
     padding=15,
 )
 btn_choose_summary_sheet.grid(row=0, column=0, padx=5, pady=5)
 
 label_summary_sheet = Label(
     frame_summary,
-    textvariable=path_summary_sheet_var,
+    textvariable=summary_sheet_path_var,
     padding=(50, 10),
     font="Arial 14 bold",
     background="yellow",
@@ -59,19 +58,21 @@ separator.pack(fill=X, padx=10, pady=10)  # 水平填充，并设置一些内边
 frame_raw = Frame(root)
 frame_raw.pack(fill=BOTH, expand=True)
 
-path_raw_sheet_var = StringVar()
-path_raw_sheet_var.set("NA")
+raw_sheet_path_var = StringVar()
+raw_sheet_path_var.set("NA")
+raw_sheet_label_var = StringVar()
+raw_sheet_label_var.set("Sheet1")
 
 btn_choose_raw_sheet = Button(
     frame_raw,
     text="选择原始数据表",
-    command=lambda: open_openfilename_dialog(path_raw_sheet_var),
+    command=lambda: open_openfilename_dialog(raw_sheet_path_var),
     padding=15,
 )
 btn_choose_raw_sheet.grid(row=0, column=0, padx=5, pady=5)
 label_raw_sheet = Label(
     frame_raw,
-    textvariable=path_raw_sheet_var,
+    textvariable=raw_sheet_path_var,
     padding=(50, 10),
     font="Arial 14 bold",
     background="yellow",
@@ -80,47 +81,79 @@ label_raw_sheet.grid(row=0, column=1, padx=5, pady=5)
 
 raw_sheet_label = Label(frame_raw, text="工作表名")
 raw_sheet_label.grid(row=1, column=0, padx=5, pady=5)
-raw_sheet_entry = Entry(frame_raw, width=30)
+raw_sheet_entry = Entry(frame_raw, width=30, textvariable=raw_sheet_label_var)
 raw_sheet_entry.grid(row=1, column=1, padx=5, pady=5)
 
 
-def start_process(path_raw_sheet_var, path_summary_sheet_var):
-    # raw_sheet_workbook = load_workbook(path_raw_sheet_var.get())
-
-    # # 选择工作表，这里假设你要读取的是第一个工作表，也可以通过名称选择
-    # sheet = raw_sheet_workbook["Sheet1"]
-
-    # # 遍历第一列的单元格（注意，行和列的索引都是从1开始的）
-    # for row in sheet.iter_rows(min_row=1, max_col=1, values_only=False):
-    #     # row 是一个元组，包含第一列的单元格对象
-    #     # row[0] 是第一列的单元格对象
-    #     cell_value = row[0].value
-    #     print(cell_value)  # 打印单元格的值
-
-    # 如果你只想获取第一列的所有值，可以使用列表推导式
-    # first_column_values = [cell.value for cell in sheet['A']]
-    # print(first_column_values)
-
-    summary_sheet_workbook = load_workbook(path_summary_sheet_var.get())
-
-    # 选择工作表，这里假设你要读取的是第一个工作表，也可以通过名称选择
+def start_process(raw_sheet_path_var, summary_sheet_path_var):
+    summary_sheet_workbook = load_workbook(summary_sheet_path_var.get())
     summary_sheet = summary_sheet_workbook[summary_sheet_label_var.get()]
 
-    encountered_valued_data = False
-    for col in summary_sheet["D"]:
-        d_column_value = col.value
-        if d_column_value is None:
-            if encountered_valued_data:
-                break
+    summary_item_number_dict = {}
+    summary_sheet_item_col = "D"
+    summary_sheet_item_row = 4
+    cur_row = 0
+    for col in summary_sheet[summary_sheet_item_col]:
+        cur_row += 1
+        if cur_row < summary_sheet_item_row:
+            continue
+
+        item_name = col.value
+        if item_name is None:
+            break
         else:
-            encountered_valued_data = True
-            print(d_column_value)
+            if item_name not in summary_item_number_dict:
+                summary_item_number_dict[item_name] = 0
+            else:
+                print(f"!! 汇总表里有重复的 !! {item_name}")
+
+    print("汇总表")
+    cur_row = 0
+    for key, value in summary_item_number_dict.items():
+        cur_row += 1
+        print(f"{cur_row}: {key} {value}")
+    print("")
+
+    #######
+    raw_sheet_workbook = load_workbook(raw_sheet_path_var.get())
+    raw_sheet = raw_sheet_workbook[raw_sheet_label_var.get()]
+
+    raw_item_number_dict = {}
+    summary_sheet_item_col_num = 3  # "D"
+    summary_sheet_number_col_num = 5  # "F"
+    summary_sheet_item_row = 5
+    cur_row = 0
+    for col in raw_sheet:
+        cur_row += 1
+        if cur_row < summary_sheet_item_row:
+            continue
+
+        item_name = col[summary_sheet_item_col_num].value
+        if item_name is None:
+            break
+        else:
+            if "非中选" not in item_name:
+                if item_name not in raw_item_number_dict:
+                    raw_item_number_dict[item_name] = col[
+                        summary_sheet_number_col_num
+                    ].value
+                else:
+                    raw_item_number_dict[item_name] = (
+                        raw_item_number_dict[item_name]
+                        + col[summary_sheet_number_col_num].value
+                    )
+
+    print("统计表")
+    cur_row = 0
+    for key, value in raw_item_number_dict.items():
+        cur_row += 1
+        print(f"{cur_row}: {key} {value}")
 
 
 btn_process = Button(
     root,
     text="开始汇总",
-    command=lambda: start_process(path_raw_sheet_var, path_summary_sheet_var),
+    command=lambda: start_process(raw_sheet_path_var, summary_sheet_path_var),
     padding=15,
 )
 btn_process.pack()
